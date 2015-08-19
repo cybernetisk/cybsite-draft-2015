4425c2abee849319faaa7c3ef44c511bde6f537e
Steffen Nilsen <steffen@ardoq.com>
2015-08-19 11:17:11 +0200

    initial upload

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,13 +1,20 @@
 (function() {
-    $(document).ready(activate);
-
-    function activate() {
+    $(document).ready(function() {
         carousel();
-    }
+        menu();
+    });
 
     function carousel() {
         $('.carousel').carousel({
             interval: 2000
+        });
+    }
+
+    function menu() {
+        $('#menu').slicknav({
+            label: '',
+            duplicate: false,
+            prependTo: '#menu-anchor'
         });
     }
 })();
